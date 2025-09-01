00c4b2586935ab13037d046110fa8eb0135efd65
Jery <jery99961@gmail.com>
2025-09-01 08:26:24 +0530

    refactor(session-options): update session options

--- lib/types.ts
+++ lib/types.ts
@@ -45,22 +45,22 @@ export type SessionOption =
   | '9.45-10.45'
   | '8.45-10.45'
   | '11-12'
-  | '12-13'
-  | '11-13'
-  | '14-14.50'
-  | '14.50-15.30'
-  | '14-15.40';
+  | '12.50-13.50'
+  | '12.50-14.50'
+  | '13.50-14.50'
+  | '13.50-15.50'
+  | '14.50-15.50';
 
 export const SESSION_OPTIONS: { key: SessionOption; value: string }[] = [
   { key: '8.45-9.45', value: '8:45 AM to 9:45 AM' },
   { key: '9.45-10.45', value: '9:45 AM to 10:45 AM' },
   { key: '8.45-10.45', value: '8:45 AM to 10:45 AM (2hrs)' },
   { key: '11-12', value: '11:00 AM to 12:00 PM' },
-  { key: '12-13', value: '12:00 PM to 1:00 PM' },
-  { key: '11-13', value: '11:00 AM to 1:00 PM (2hrs)' },
-  { key: '14-14.50', value: '2:00 PM to 2:50 PM' },
-  { key: '14.50-15.30', value: '2:50 PM to 3:30 PM' },
-  { key: '14-15.40', value: '2:00 PM to 3:40 PM (2hrs)' }
+  { key: '12.50-13.50', value: '12:50 PM to 1:50 PM' },
+  { key: '12.50-14.50', value: '12:50 PM to 2:50 PM (2hrs)' },
+  { key: '13.50-14.50', value: '1:50 PM to 2:50 PM' },
+  { key: '13.50-15.50', value: '1:50 PM to 3:50 PM (2hrs)' },
+  { key: '14.50-15.50', value: '2:50 PM to 3:50 PM' },
 ];
 
 export const SECTION_MAPPINGS: { [key: string]: string } = {
